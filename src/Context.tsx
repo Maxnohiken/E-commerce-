9ae7c75a3442b84ee4c546cb2796b103881cf329
Pie9706 <pietro.montalbano9706@gmail.com>
2024-03-22 12:32:39 +0100

    First

---
+++ src/Context.tsx
@@ -0,0 +1,39 @@
+import { createContext, ReactNode, useState } from "react";
+import { TContext, Product } from "./declarations";
+
+export const AppContext = createContext<TContext>({
+  username: "",
+  products: [],
+  cart: [],
+  paid: false,
+  addToCart: () => {},
+  removeFromCart: () => {},
+  checkout: () => {},
+  getTotalProductInCart: () => 0,
+  onCheckoutSuccess: () => {},
+  getTotalAvailableProduct: () => 0,
+  login: () => {},
+  logout: () => {},
+  searchTerm: "",
+  setSearchTerm: () => {},
+  filteredProducts: [],
+  handleSearchChange: () => {},
+});
+
+interface Props {
+  children: ReactNode;
+}
+
+const utilityGetPreviousUsername = () => {
+  const username = localStorage.getItem("username");
+  return username || "";
+};
+export function ContextProvider({ children }: Props) {
+  const [products, setProducts] = useState<Array<Product>>([]);
+  const [cart, setCart] = useState<Array<{ prod: Product; qty: number }>>([]);
+  const [paid, setPaid] = useState<boolean>(false);
+  const initUsername = utilityGetPreviousUsername();
+  const [username, setUsername] = useState<string>(initUsername);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>(products);
+}
---
+++ src/declarations.tsx
@@ -0,0 +1,27 @@
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  qty: number;
+  image: string;
+  description: string;
+}
+
+export interface TContext {
+  username: string;
+  products: Array<Product>;
+  cart: Array<{ prod: Product; qty: number }>;
+  paid: boolean;
+  addToCart: (product: Product, num: number) => void;
+  removeFromCart: (idProduct: Product["id"]) => void;
+  checkout: () => void;
+  getTotalProductInCart: () => number;
+  onCheckoutSuccess: () => void;
+  getTotalAvailableProduct: (product: Product) => number;
+  login: (username: string) => void;
+  logout: () => void;
+  searchTerm: string; // Aggiungi searchTerm al tipo TContext
+  setSearchTerm: (term: string) => void; // Aggiungi setSearchTerm al tipo TContext
+  filteredProducts: Product[]; // Aggiungi filteredProducts al tipo TContext
+  handleSearchChange: (event: React.ChangeEvent<HTMLInputElement>) => void; // Aggiungi handleSearchChange al tipo TContext
+}

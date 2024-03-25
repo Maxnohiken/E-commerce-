6946babb78ec3a94a5601ab794e085d1b05ff5df
Massimiliano Arena <milikkan5000@gmail.com>
2024-03-25 14:30:28 +0100

    feat/completamento interfaccia home

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Grid, TextField } from "@mui/material";
+import { Container, Grid, TextField } from "@mui/material";
 import { AppContext } from "../Context";
 import { useContext } from "react";
 import { Card } from "../components/card";
@@ -6,40 +6,32 @@ import DrawerAppBar from "../components/navbarComponent";
 import Unsplash from "../components/unsplash";
 
 export function RouteHome() {
-
   const { filteredProducts, paid, handleDeleteProduct, searchTerm, handleSearchChange } = useContext(AppContext);
-
 
   return (
     <>
       {!paid && <DrawerAppBar />}
-
       <Unsplash />
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flex: "1",
-        }}
-      >
-        <TextField
-          label=""
-          placeholder="Ricerca Prodotto"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          style={{
-            backgroundColor: "white",
-            color: "white",
-            width: "400px",
-            maxWidth: "50%",
-          }}
-          value={searchTerm}
-          onChange={handleSearchChange}
-        />
-
-      </Box>
+      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", width:"85%", margin:"auto" }}>
+        <h2 style={{ textAlign: "right", color: "black", marginRight: "auto" }}>Prodotti</h2>
+        <div style={{ flex: "1", display: "flex", justifyContent: "flex-end", alignItems: "center" }}>
+          <TextField
+            label=""
+            placeholder="Ricerca Prodotto"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            style={{
+              backgroundColor: "white",
+              width: "200px",
+              height: "40px",
+              maxWidth: "50%",
+            }}
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+        </div>
+      </div>
       <Container maxWidth="lg">
         <Grid container spacing={2}>
           {filteredProducts.map((product) => (

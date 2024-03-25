87d2f3eab2513270d7e830e107d6ee3241494029
Francesco Pizzino <ciccio@MacBook-Pro-di-Francesco.local>
2024-03-25 12:00:27 +0100

    (feat project): Added unsplash image

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -3,38 +3,45 @@ import { AppContext } from "../Context";
 import { useContext } from "react";
 import { Card } from "../components/card";
 import DrawerAppBar from "../components/navbarComponent";
+import Unsplash from "../components/unsplash";
 
 export function RouteHome() {
-  const { filteredProducts, paid, handleDeleteProduct,searchTerm, handleSearchChange } =
-    useContext(AppContext);
+  const {
+    filteredProducts,
+    paid,
+    handleDeleteProduct,
+    searchTerm,
+    handleSearchChange,
+  } = useContext(AppContext);
 
   return (
     <>
       {!paid && <DrawerAppBar />}
+      <Unsplash />
       <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              flex: "1",
-            }}
-          >
-            <TextField
-              label=""
-              placeholder="Ricerca Prodotto"
-              variant="outlined"
-              fullWidth
-              margin="normal"
-              style={{
-                backgroundColor: "white",
-                color: "white",
-                width: "400px",
-                maxWidth: "50%",
-              }}
-              value={searchTerm}
-              onChange={handleSearchChange}
-            />
-          </Box>
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flex: "1",
+        }}
+      >
+        <TextField
+          label=""
+          placeholder="Ricerca Prodotto"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          style={{
+            backgroundColor: "white",
+            color: "white",
+            width: "400px",
+            maxWidth: "50%",
+          }}
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+      </Box>
       <Container maxWidth="lg">
         <Grid container spacing={2}>
           {filteredProducts.map((product) => (
